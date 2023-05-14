517d8e0f76cea726c461350a7fd1b4b39ce9b8b7
Nissan Ahmed <aan007bd@gmail.com>
2023-05-14 18:34:21 +0600

    Rewrite the styles in Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ni554n.github.io",
-  integrations: [tailwind()],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false,
+    },
+  })],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,19 @@
+const defaultTailwindTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
   theme: {
+    colors: {
+      white: "#FFF",
+      black: "#000",
+      gray: {
+        700: "#616161",
+        900: "#212121",
+      },
+      gold: "#AD8715",
+      rose: "#B78",
+    },
     extend: {
       fontFamily: {
         mono: ["Fira Code", ...defaultTailwindTheme.fontFamily.mono],
