4bb0feee87f9555b8f29e723549d5cd78c216e91
Nissan Ahmed <aan007bd@gmail.com>
2023-05-15 20:26:59 +0600

    Replace hardcoded analytics info with envs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_UMAMI_SCRIPT_URL: string;
+  readonly PUBLIC_UMAMI_WEBSITE_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
